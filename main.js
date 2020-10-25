ce2675297a8bbba3cf230cfda5c43f910ccfeaf7
Shawn Carter <53487411+ShawnCart3r@users.noreply.github.com>
2020-10-25 16:53:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    autoscrolling: true,
+    navigation: true,
+    anchors: ['section1', 'section2', 'section3','section4'],
+    navigationTooltips: ['Home', 'About', 'Work', 'Contact'],
+    showActiveTooltip: true,
+    scrollingSpeed: 1000,
+})
